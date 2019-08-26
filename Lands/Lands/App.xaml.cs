324807afcbef0cde96ca1011b2dc51be4195bbd2
Marco Oscco <osccoem@gmail.com>
2019-08-26 12:37:41 -0500

    Inicio del proyecto de Xamarin

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using Xamarin.Forms.Xaml;
+
+namespace Lands
+{
+    using Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+    {
+        #region Constructors
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage( new LoginPage() );
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
